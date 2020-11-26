3691bc51a779289731616e27604bae99eccb12db
Vanessa <vannexxaa@gmail.com>
2020-11-26 21:54:34 +0000

    Measures the size of a binary tree

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,23 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * enume - Counts the size.
- * @node: Pointer to node of the tree.
- * @size: Size of the tree.
- *
- * Return: Nothing.
- */
-void enume(const binary_tree_t *node, size_t *size)
-{
-	if (node)
-	{
-		*size = *size + 1;
-		enume(node->left, size);
-		enume(node->right, size);
-	}
-}
-
-/**
  * binary_tree_size - Measures the size of a binary tree.
  * @tree: Pointer to the root node of the tree to measure the size.
  *
@@ -28,14 +11,12 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	size_t size = 0;
 
 	if (!tree)
-	{
 		return (0);
-	}
-	else
+	if (tree)
 	{
 		size = size + 1;
-		enume(tree->left, &size);
-		enume(tree->right, &size);
+		size = size + binary_tree_size(tree->left);
+		size = size + binary_tree_size(tree->right);
 	}
 	return (size);
 }
