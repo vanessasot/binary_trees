bc8fbd12285f4c0f14cbc7295d0dee7fafb8684f
Vanessa <vannexxaa@gmail.com>
2020-11-26 21:28:45 +0000

    Add prototype enum_leaf

--- binary_trees.h
+++ binary_trees.h
@@ -41,10 +41,9 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_height(const binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *tree);
-
 void enume(const binary_tree_t *node, size_t *size);
 size_t binary_tree_size(const binary_tree_t *tree);
-
+void enum_leaf(const binary_tree_t *node, size_t *leaves);
 size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
@@ -52,5 +51,7 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+
+void binary_tree_print(const binary_tree_t *tree);
 
 #endif /* BINARY_TREES */
