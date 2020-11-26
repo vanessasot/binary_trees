f80794e43d6aa1f9f87665b3faab9e75af7c0770
Vanessa <vannexxaa@gmail.com>
2020-11-26 20:11:44 +0000

    Add prototype enume

--- binary_trees.h
+++ binary_trees.h
@@ -42,7 +42,7 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_height(const binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *tree);
 
-void enum(const binary_tree_t *node, size_t *size);
+void enume(const binary_tree_t *node, size_t *size);
 size_t binary_tree_size(const binary_tree_t *tree);
 
 size_t binary_tree_leaves(const binary_tree_t *tree);
